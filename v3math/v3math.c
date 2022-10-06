35a827f4c5a75576c597c1b019d3050706e99ead
Noah Nannen <ncn23@nau.edu>
2022-10-06 10:26:26 -0700

    Update v3math.c

--- v3math/v3math.c
+++ v3math/v3math.c
@@ -1,4 +1,5 @@
 #import v3math.h
+#import <math.h>
 
 
 void v3_from_points(float *dst, float *a, float *b) {
@@ -33,19 +34,39 @@ void v3_cross_product(float *dst, float *a, float *b) {
 
 void v3_scale(float *dst, float s) {
 
-
+    dst[0] = dst[0] * s;
+    dst[1] = dst[1] * s;
+    dst[2] = dst[2] * s;
 
 }
 
 float v3_angle(float *a, float *b) {
 
-    return 0;
+    float lengthA = v3_length(*a);
+    float lengthB = v3_length(*b);
+
+    float numerator = (a[0] * b[0]) + (a[1] * b[1]) + (a[2] * b[2]);
+    float denominator = lengthA * lengthB;
+    
+    float angle = numerator / denominator;
+
+    float cosAngle = acos(angle);
+
+    return cosAngle
 
 }
 
 float v3_angle_quick(float *a, float *b) {
 
-    return 0;
+    float lengthA = v3_length(*a);
+    float lengthB = v3_length(*b);
+
+    float numerator = (a[0] * b[0]) + (a[1] * b[1]) + (a[2] * b[2]);
+    float denominator = lengthA * lengthB;
+
+    float angle = numerator / denominator;
+
+    return angle;
 
 }
 
@@ -57,12 +78,13 @@ void v3_reflect(float *dst, float *v, float *n) {
 
 float v3_length(float *a) {
 
+    float length = sqrt(a[0]^2 + a[1]^2 + a[2]^2);
 
-
+    return length;
 }
 
 void v3_normalize(float *dst, float *a) {
 
-    
+
 
 }
