36afea7ac6b4d8db8c2cb33e305ea83d85f574bf
Johnathan Ray <jdr479@nau.edu>
2022-10-18 16:11:33 -0700

    Fixed ppm project stuff

--- ppmrw.c
+++ ppmrw.c
@@ -11,74 +11,53 @@
 #include <string.h>
 #include <stdbool.h>
 
-void readFromP3(FILE* fh, int size, uint32_t *asciiArray) {
+void read_p3(FILE * fh, uint8_t *image) {
     int index = 0;
-    int scanCount = 1;
-    while (!feof(fh) && scanCount == 1) {
-        scanCount = fscanf(fh, "%u", &asciiArray[index]); 
-        index++;
-    }
-}
-
-void readFromP6(FILE* fh, int size, uint8_t *binaryArray) {
-    fread(&binaryArray, sizeof(uint8_t), size, fh);
-}
-
-
-void skipComment(FILE* fh) {
-    char currentChar[1];
-    while (currentChar[0] != '\n') {
-        fread(currentChar, 1, 1, fh);
-    }
-}
-
-void writeToP3FromP3(FILE* fh, int width, int size, uint32_t outputArray[]) {
-
-    int index;
-
-    for (index = 0; index < size; index++) {
-        
-        fprintf(fh, "%d %d %d ", outputArray[index], outputArray[index+1], outputArray[index+2]);\
-
+    int scanCount = 3;
+    while (!feof(fh) && scanCount == 3) {
+        int r;
+        int g;
+        int b;
+        scanCount = fscanf(fh, "%u %u %u", &r, &g, &b);
+        image[index] = r;
+        image[index+1] = g;
+        image[index+2] = b;
         index += 3;
-
-        if ((index / 3) == width) {
-
-            fprintf(fh, "\n");
-
-        }
     }
 }
 
-void writeToP3FromP6(FILE* fh, int width, int size, uint8_t outputArray[]) {
-
-    int index;
-    for (index = 0; index < size; index++) {
-
-        fprintf(fh, "%d %d %d ", outputArray[index], outputArray[index+1], outputArray[index+2]);
-
-        index += 3;
-
-        if ((index / 3) == width) {
-
-            fprintf(fh, "\n");
-
-        }
+void read_p6(FILE* fh, int width, int height, uint8_t *image) {    
+    for (int i = 0; i < width * height * 3; i++) {
+        fread(&image[i], sizeof(uint8_t), 1, fh);
     }
 }
 
-void writeToP6FromP3(FILE* fh, int size, uint32_t outputArray[]) {
 
-    fwrite(outputArray, sizeof(uint8_t), size, fh);
+void skipWS(FILE* fh) {
+    char currentChar;
+    fscanf(fh, "%c", &currentChar);
 }
 
+void write_p3(FILE* fh, int width, int height, int maxcol, uint8_t *image) {
+    fprintf(fh, "P3 %d %d %d\n", width, height, maxcol);
+    printf("Writing stuff...");
 
-void writeToP6FromP6(FILE* fh, int size, uint8_t outputArray[]) {
-    
-    fwrite(outputArray, sizeof(uint8_t), size, fh);
+    for (int index = 0; index < width * height * 3 ; index += 3) {
+        int val1 = image[index];
+        int val2 = image[index + 1];
+        int val3 = image[index + 2];
+        fprintf(fh, "%d %d %d \n", val1, val2, val3);
+    }
 }
 
-int main (int argc, char **argv) {
+void write_p6(FILE* fh, int width, int height, int maxcol, uint8_t* image) {
+
+    fprintf(fh, "P6 %d %d %d\n", width, height, maxcol);
+    fwrite(image, sizeof(uint8_t), width * height * 3, fh);
+
+}
+
+int main(int argc, char** argv) {
 
     int width = 0, height = 0;
     int maxColorVal = 0;
@@ -86,123 +65,65 @@ int main (int argc, char **argv) {
     // check if command line has missing arguments
     if (argc != 4) {
         fprintf(stderr, "Error: Missing/too many arguments on command line");
-	    return 7;
+        return 7;
     }
 
     // open the input file
-    FILE *inputfh = fopen(argv[2], "r");
-    FILE *outputfh = fopen(argv[3], "w");
+    FILE* inputfh = fopen(argv[2], "r");
 
     // check if input file opened incorrectly
     if (inputfh == NULL) {
         fprintf(stderr, "Error: Input file not found");
-	    return 2;
+        return 2;
     }
 
-    // check if output file opened incorrectly
+    // MAGIC NUMBER
+    int c = fgetc(inputfh);
+    if (c != 'P') {
+        fprintf(stderr, "Error: magic number not found\n");
+        return 3;
+    }
+    char inputFormat = fgetc(inputfh);
+    if ((inputFormat != '3') && (inputFormat != '6')) {
+        fprintf(stderr, "Error: magic number not found\n");
+        return 3;
+    }
+
+    // Read width, height, and max color val; skip whitespaces/comments
+    skipWS(inputfh);
+    fscanf(inputfh, "%d", &width);
+    
+    skipWS(inputfh);
+    fscanf(inputfh, "%d", &height);
+    
+    skipWS(inputfh);
+    fscanf(inputfh, "%d", &maxColorVal);
+    skipWS(inputfh);
+
+    uint8_t image[width * height * 3];
+    // Read input file depending on format
+    if (inputFormat == '3') {
+        read_p3(inputfh, image);
+    }
+    else {
+        printf("%d\t%d\n", width, height);
+        read_p6(inputfh, width, height, image);
+        printf("%d\t%d\n", width, height);
+    }
+
+    // Write to output file depending on format
+    int outputFormat = atoi(argv[1]);
+    FILE* outputfh = fopen(argv[3], "w");
     if (outputfh == NULL) {
         fprintf(stderr, "Error: output file not found");
-	    return 2;
+        return 2;
     }
 
-// MAGIC NUMBER
-
-    // scan to find the magic number
-    char magicNumStr[] = "";
-
-    char currentChar[1];
-    char magicNumChar[2];
-    bool skippedComment = false;
-
-    while (!skippedComment) {
-        fread(currentChar, 1, 1, inputfh);
-        if (strcmp(currentChar, "P") == 0) {
-            skippedComment = true;
-        }
-        else {
-            if (ispunct(currentChar[0])) {
-                skipComment(inputfh);
-            }
-            else {
-                fprintf(stderr, "Error: Magic number not found in input file\n");
-                return 3;
-            }
-        }
+    if (outputFormat == 3) {
+        write_p3(outputfh, width, height, maxColorVal, image);
     }
-    
-    fread(magicNumChar, 1, 1, inputfh);
-    
-    // Write the magic number to the output file
-    sprintf(magicNumStr, "P%s", argv[1]);
-    fwrite(magicNumStr, sizeof(magicNumStr), strlen(magicNumStr), outputfh);
-
-    // display final message
-    printf("\nConversion Complete\n");
-    
-// DIMENSIONS
-
-    // scan in the dimensions of the image
-    char widthAndHeight[] = "";
-    int scanCount = fscanf(inputfh, "\n%d %d\n", &width, &height);
-
-    int size = width * height * 3;
-
-    // check if width and height not found
-    if (scanCount == 0) {
-        fprintf(stderr, "Error: width and height not found");
-        return 3;
-    }
-    
-    sprintf(widthAndHeight, "\n%d %d\n", width, height);
-    
-    // write out the dimensions
-    fwrite(widthAndHeight, sizeof(widthAndHeight), strlen(widthAndHeight), outputfh);
-    printf("\nWidth and height written");
-
-// MAX COLOR VALUE
-
-    // scan in the max color value
-    char maxColorValStr[] = "";
-    scanCount = fscanf(inputfh, "%d\n", &maxColorVal);
-
-    // check if max color value not found
-    if (scanCount == 0) {
-        fprintf(stderr, "Error: max color value not found");
-        return 3;
-    }
-    sprintf(maxColorValStr, "%d\n", maxColorVal);
-
-    // write out the max color value
-    fwrite(maxColorValStr, sizeof(maxColorValStr), strlen(maxColorValStr), outputfh);
-    printf("\nMax color val written\n");
-
-// READ AND WRITE COLOR VALUES
-    
-    // check what file is being read from
-    uint32_t asciiArray[size];
-    uint8_t binaryArray[size];
-    
-    if (strcmp(magicNumChar, "3") == 0) {
-        readFromP3(inputfh, size, asciiArray);
-
-        // check what file to write to
-        if (strcmp(argv[1], "6") == 0) {
-            writeToP6FromP3(outputfh, size, asciiArray);
-        }
-        else if (strcmp(argv[1], "3") == 0) {
-            writeToP3FromP3(outputfh, width, size, asciiArray);
-        }
-    }
-    else if (strcmp(magicNumChar, "6") == 0) {
-        readFromP6(inputfh, size, binaryArray);
-
-        // check what file to write to
-        if (strcmp(argv[1], "6") == 0) {
-            writeToP6FromP6(outputfh, size, binaryArray);
-        }
-        else if (strcmp(argv[1], "3") == 0) {
-            writeToP3FromP6(outputfh, width, size, binaryArray);
-        }
+    else {
+        write_p6(outputfh, width, height, maxColorVal, image);
     }
 
     // close input and output files
